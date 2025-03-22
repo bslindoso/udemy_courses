83ca85d779642f01c94816ebdaddbb59b87f66b0
Bruno Lindoso <48108317+bslindoso@users.noreply.github.com>
2025-03-21 22:22:54 -0300

    revisando objetos inicio

---
+++ js_ts_do_basico_ao_avancado/05_js_objetos_e_prototypes/aula_089_.revisando_objetos/revisao.js
@@ -0,0 +1,19 @@
+// usando notação de chaves
+const pessoa = {
+  nome: "Luiz",
+  sobrenome: "Otávio",
+  "data de nascimento": new Date(1980, 3, 12).toLocaleDateString()
+}
+console.log(pessoa, pessoa.nome, pessoa.sobrenome, pessoa["data de nascimento"]);
+
+// usando contrutor
+const pessoa1 = new Object();
+pessoa1.nome = 'Maria';
+pessoa1.sobrenome = 'Bairro';
+pessoa1["data de nascimento"] = new Date(1980, 3, 12).toLocaleDateString();
+console.log(pessoa1, pessoa1.nome, pessoa1.sobrenome, pessoa1["data de nascimento"]);
+
+// deletando uma propriedade
+delete pessoa1["data de nascimento"];
+console.log(pessoa1, pessoa1.nome, pessoa1.sobrenome, pessoa1["data de nascimento"]);
+
